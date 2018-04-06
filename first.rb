0b206ca215d79a356408701a764bd2ede7901e74
Gabriel <gabrielomar2809@gmail.com>
2018-04-06 18:26:40 -0400

    Hello world

---
+++ first.rb
@@ -0,0 +1,7 @@
+# Calculate the sum of two numbers
+puts "Hello world, we are going to calculate the sum of two numbers"
+puts "Enter the first number:"
+num1 = gets.to_i
+puts "Enter the second number:"
+num2 = gets.to_i
+puts "The sum is "+(num1 + num2).to_s
