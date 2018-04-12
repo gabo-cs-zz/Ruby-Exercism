1e6edb4f6d451c69d2880fa2b6875ee58a099bc0
Gabriel <gabrielomar2809@gmail.com>
2018-04-12 12:29:52 -0400

    Interpolation added. Line 8

--- first.rb
+++ first.rb
@@ -1,15 +1,16 @@
 # Calculate the sum of two numbers
-=begin
+
 puts "Hello world, we are going to calculate the sum of two numbers"
 puts "Enter the first number:"
 num1 = gets.to_i
 puts "Enter the second number:"
 num2 = gets.to_i
-puts "The sum is "+(num1 + num2).to_s
-=end
+puts "The sum is #{num1 + num2}"
+
 
 
 # Calculate the factorial of a number:
+=begin
 puts "Enter a number to calculte its factorial:"
 num = gets.to_i
 
@@ -20,6 +21,25 @@ def factorial(n)
     return n * factorial(n-1)
   end
 end
+
 puts factorial(num)
+=end
 
 # Calculate the sum of the first 15 numbers of the fibonnacci sequence:
+
+# Calculate the summation of a number x (from 1 to n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
