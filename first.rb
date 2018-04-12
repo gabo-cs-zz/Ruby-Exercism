3f16dd96b61fdc377b9704ad3e4b96019a3e625b
Gabriel <gabrielomar2809@gmail.com>
2018-04-12 14:34:13 -0400

    Playing around with the fibonacci sequence

--- first.rb
+++ first.rb
@@ -1,15 +1,14 @@
-# Calculate the sum of two numbers
 =begin
+# Calculate the sum of two numbers
 puts "Hello world, we are going to calculate the sum of two numbers"
 puts "Enter the first number:"
 num1 = gets.to_i
 puts "Enter the second number:"
 num2 = gets.to_i
 puts "The sum is #{num1 + num2}"
-=end
+
 
 # Calculate the factorial of a number:
-=begin
 puts "Enter a number to calculate its factorial:"
 num = gets.to_i
 
@@ -22,7 +21,6 @@ def factorial(n)
 end
 
 puts factorial(num)
-=end
 
 # Calculate the summation of a number x (from 1 to x)
 puts "Enter a number to calculte its summation:"
@@ -37,3 +35,24 @@ def summation(n)
 end
 
 puts summation(x)
+=end
+
+# Calculate the sum of the 15 first digits of the fibonacci sequence
+aux, aux2, temp, i = 0, 1, 0, 1
+nums = Array.new
+nums.push(temp)
+until i > 15
+  aux = aux2
+  aux2 = temp
+  temp = aux + aux2
+  nums.push(temp)
+  i += 1
+end
+puts "These are the 15 first digits:"
+p nums
+puts "And the sum of these numbers is:"
+sum = 0
+nums.each do | k |
+  sum += k
+end
+puts sum
