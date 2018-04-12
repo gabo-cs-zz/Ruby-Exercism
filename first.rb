efb26d68c8749cf4652fe0e770f71f8207d077ce
Gabriel <gabrielomar2809@gmail.com>
2018-04-12 12:50:17 -0400

    Summation (from 1 to n) added

--- first.rb
+++ first.rb
@@ -1,17 +1,16 @@
 # Calculate the sum of two numbers
-
+=begin
 puts "Hello world, we are going to calculate the sum of two numbers"
 puts "Enter the first number:"
 num1 = gets.to_i
 puts "Enter the second number:"
 num2 = gets.to_i
 puts "The sum is #{num1 + num2}"
-
-
+=end
 
 # Calculate the factorial of a number:
 =begin
-puts "Enter a number to calculte its factorial:"
+puts "Enter a number to calculate its factorial:"
 num = gets.to_i
 
 def factorial(n)
@@ -25,21 +24,13 @@ end
 puts factorial(num)
 =end
 
-# Calculate the sum of the first 15 numbers of the fibonnacci sequence:
-
-# Calculate the summation of a number x (from 1 to n)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Calculate the summation of a number x (from 1 to x)
+puts "Enter a number to calculte its summation:"
+x = gets.to_i
+i = 1
+sum = 0
+until i > x do
+  sum += i
+  i+=1
+end
+puts sum
