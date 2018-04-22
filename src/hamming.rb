245fe894022d34cbc0677b90b82b4918a56187f4
Gabriel <gabrielomar2809@gmail.com>
2018-04-22 15:15:27 -0400

    Some changes

--- src/hamming.rb
+++ src/hamming.rb
@@ -1,28 +1,25 @@
 # Calculate the Hamming difference between two DNA strands.
-
-puts "Enter the first DNA strand: "
-st1 = gets.upcase
-puts "Enter the second DNA strand: "
-st2 = gets.upcase
-str1, str2 = st1.split(''), st2.split('')
-str1.pop
-str2.pop
-res = Array.new # Indication signs
-if st1.size != st2.size
-  puts "The two strands must have the same length"
-else 
-  i, s = 0, 0
-  while i < (str2.length) do
-    if !(str1[i].eql?(str2[i]))
+def ham_dif(st1, st2)
+  res, i, s = [], 0, 0
+  until i > st1.size do
+    if st1[i] != st2[i]
       s += 1
       res.push('^')
+    else 
+      res.push(' ')
     end
-    res push('') 
     i += 1
   end
-  puts "Here is your result:"
-  puts st1
-  puts st2
-  # puts "#{res.join('')}" -- Here is where the indication signs would be.
-  puts "The Hamming distance between these two DNA strands is #{s}."
+  puts 'Here is your result:', st1, st2, "#{res.join}"
+  puts "The Hamming Distance between these two DNA strands is #{s}."
+end
+
+puts 'Enter the first DNA strand: '
+st1 = (gets.chomp).upcase
+puts 'Enter the second DNA strand: '
+st2 = (gets.chomp).upcase
+unless st1.size == st2.size
+  puts 'The strands must have the same length.'
+else 
+  ham_dif(st1, st2)
 end
