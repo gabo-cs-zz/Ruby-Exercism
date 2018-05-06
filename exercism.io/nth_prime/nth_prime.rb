17b46f90025149e95ef0eb2260ed2e580bdecec1
Gabriel <gabrielomar2809@gmail.com>
2018-05-06 16:31:59 -0400

    Nth Prime

--- exercism.io/nth_prime/nth_prime.rb
+++ exercism.io/nth_prime/nth_prime.rb
@@ -1,18 +1,19 @@
 # Given a number n, determine what the nth prime is.
 class Prime
-	def self.nth (num)
-		raise ArgumentError unless num != 0
-		primes = [2]
-		i = 1
-		until primes.size == num do
-			c = 0
-			(1..i).each do |j|
-				break if c > 2
-				c+= 1 if i % j == 0
-			end
-			primes << i if c == 2
-			i+=2
-		end
-		primes[-1]
-	end
+  def self.nth(num)
+    raise ArgumentError unless num != 0
+    primes = [2]
+    i = 3
+    until primes.size >= num
+      primes << i if prime?(primes, i)
+      i += 2
+    end
+    primes[-1]
+  end
+
+  def self.prime?(primes, num)
+    root = Math.sqrt(num)
+    p = primes.take_while { |i| i <= root }
+    p.none? { |i| (num % i).zero? }
+  end
 end
