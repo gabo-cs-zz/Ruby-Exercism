3dac8b256992f35c64eeaa4cbf99c7b04b361dff
Gabriel <gabrielomar2809@gmail.com>
2018-06-12 16:20:39 -0400

    Working with controllers, partials, helpers..

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def render_stars(value)
+    output = ''
+    if (1..5).include?(value)
+      value.times { output += '*' }
+    end
+    output
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'example/test'
+	#root 'example#test'
+	get 'example/test'
+	# resources :test, controller: 'example'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
