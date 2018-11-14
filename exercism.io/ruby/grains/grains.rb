46e73bb256a576e10d5ee1ce6b51d4d08e5fe1ca
gcoronado28 <gabrielomar2809@gmail.com>
2018-11-14 15:55:24 +0000

    Grains improved

--- exercism.io/ruby/grains/grains.rb
+++ exercism.io/ruby/grains/grains.rb
@@ -4,12 +4,13 @@ end
 # Calculate the number of grains of wheat on a chessboard
 # given that the number on each square doubles.
 class Grains
+  SQUARES = (1..64)
   def self.square(num)
-    raise ArgumentError unless num.between?(1, 64)
+    raise ArgumentError unless SQUARES.cover? num
     2**(num - 1)
   end
 
   def self.total
-    (square(64) * 2) - 1
+    square(SQUARES.max) * 2 - 1
   end
 end
